f686116edae7ef1d2bf5ac3b1756649e3b9137de
xakimov04 <mardonhakimov0404@gmail.com>
2024-05-27 15:58:05 +0500

    nima

--- lib/logic/controller.dart
+++ lib/logic/controller.dart
@@ -10,6 +10,6 @@ class Controller extends StatefulWidget {
 class _ControllerState extends State<Controller> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp();
   }
 }
